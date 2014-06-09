1856a6488a98f43522cf8a8f70b4003d68ebd41d
ValentinCvetkovski <valentin_c00@hotmail.com>
2014-06-09 11:36:37 +0200

    få ut rätt artikel när man klickar på det

--- php/read.php
+++ php/read.php
@@ -4,6 +4,22 @@ include_once("classes/db_connect.php");
 $myDbConnect = new CRUD();
 $myDbConnect->dbConnect();
 
+$idx = $_POST['idx'];
+
+// säkerthets variabel för att echo:a ut rätt data
+$type = $_POST['type'];
+
+// fråga = visa all information i person
+$clickedArticle = json_encode(
+  $myDbConnect->select(
+  	 'SELECT * FROM articles WHERE id="'.$idx.'"'
+  )
+);
+
+if ( $type === 'thisArticle' ){
+	echo($clickedArticle);
+}
+
 // Detta är vad jag tror hur det kommer fungera ->
 // Jag tror vi får ställa flera frågor här nedan sedan så på frontend kallar vi på vilka frågor som ska visas?
 
@@ -13,4 +29,15 @@ $show = json_encode(
     'SELECT * FROM articles'
   )
 );
-echo($show);
+if ( $type === 'showAll' ){
+	echo($show);
+}
+
+
+/*
+$values = array();
+$values['first'] => 1;
+$values['second'] => $clickedArticle;
+$values['third'] => 3;
+print json_encode($values);
+*/
