a63a86a57d8843202193b326942396f3cb65a4b4
connyz <connymartinsson@yahoo.se>
2014-06-24 15:04:18 +0200

    Made frontpage article headlines blue

--- script/frontpage.js
+++ script/frontpage.js
@@ -35,7 +35,7 @@ $(function(){
 		for(var i = 0; i < data.length; i++){
 			//console.log(data[i]);
 			article += '<div class="row">' +
-				"<div class='col-sm-6 col-md-6'><h3>" + data[i].title + "</h3></div>" +
+				"<div class='col-sm-6 col-md-6'><h3 style='color:#428bca'>" + data[i].title + "</h3></div>" +
 				"<div class='col-sm-6 col-md-6 text-right'><h3>" + data[i].publicationDate + "</h3></div>" +
 				"<p class='col-xs-12'>" + data[i].summary + "</p>" +
 				'<p class="article-id">' + data[i].id + "</p>" +
