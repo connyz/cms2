5db005e27d6cb40fdbbad8a8cff019ef853ff0a3
connyz <connymartinsson@yahoo.se>
2014-06-24 14:57:03 +0200

    Fixat så att det ser ut som snygga blogginlägg på frontpagen. Dock verkar inte artiklar med åäö fungera att hämta från db och då blir artikeln null.

--- script/frontpage.js
+++ script/frontpage.js
@@ -4,10 +4,10 @@ $(function(){
 	readData();
 	//clickEvents();
 
-	var adminContent = '<h3>Articles</h3><div class="admin-articles"></div>';
+	var frontContent = '<h2 class="text-center">This is the Amazing Blog</h2><div class="front-articles"></div>';
 
     // Add content to page
-    $('#maincontent').append(adminContent);
+    $('#maincontent').append(frontContent);
 
 	function readData(){
 		$.ajax({
@@ -29,30 +29,58 @@ $(function(){
 	function tableOfDataAdmin(data){
 		//console.log("from the function tableOfDataAdmin ",data);
 
-		var tableOfTheArticles ='<table class="col-xs-12">';
-		tableOfTheArticles += "<thead><tr>" +
-			"<th>Publication Date</th>" +
-			"<th>Article</th>" +
-			"<th>Options</th>" +
-			"</tr></thead>";
+		var article ='<articles class="col-xs-12">';
+
 		// loop through result array
 		for(var i = 0; i < data.length; i++){
 			//console.log(data[i]);
-			tableOfTheArticles += '<tr>' +
-				"<td>" + data[i].publicationDate + "</td>" +
-				"<td>" + data[i].title + "</td>" +
-				'<td class="article-id">' + data[i].id + "</td>" +
-				"<td><button id='editB'>Edit</button><button id='deleteB'>Delete</button></td>" +
-				"</tr>";
-
-		// ta fram id
-		//console.log( data[i].id );
+			article += '<div class="row">' +
+				"<div class='col-sm-6 col-md-6'><h3>" + data[i].title + "</h3></div>" +
+				"<div class='col-sm-6 col-md-6 text-right'><h3>" + data[i].publicationDate + "</h3></div>" +
+				"<p class='col-xs-12'>" + data[i].summary + "</p>" +
+				'<p class="article-id">' + data[i].id + "</p>" +
+				"</div><hr>";
     }
 
-	tableOfTheArticles += "</table>";
+	article += "</articles";
 	// add the html to the dom
 	// console.log(html);
-	$('.admin-articles').html(tableOfTheArticles);
+	$('.front-articles').html(article);
   }
 
 }); // End of "Wait for DOM"
+/*
+<article>
+    <h2><a href="singlepost.html">Aries Sun Sign March 21 - April 19</a></h2>
+
+    <div class="row">
+        <div class="group1 col-sm-6 col-md-6">
+            <span class="glyphicon glyphicon-folder-open"></span>  <a href="#">Signs</a>
+                     <span class="glyphicon glyphicon-bookmark"></span> <a href="#">Aries</a>,
+                <a href="#">Fire</a>, <a href="#">Mars</a>
+        </div>
+        <div class="group2 col-sm-6 col-md-6">
+                <span class="glyphicon glyphicon-pencil"></span> <a href="singlepost.html#comments">20 Comments</a>
+                  <span class="glyphicon glyphicon-time"></span> August 24, 2013 9:00 PM
+        </div>
+    </div>
+
+    <hr>
+
+    <img src="http://placehold.it/900x300" class="img-responsive">
+
+    <br />
+
+    <p class="lead">Aries is the first sign of the zodiac, and that's pretty much how those born under this sign see themselves: first. Aries are the leaders of the pack, first in line to get things going. Whether or not everything gets done is another question altogether, for an Aries prefers to initiate rather than to complete. Do you have a project needing a kick-start? Call an Aries, by all means. The leadership displayed by Aries is most impressive, so don't be surprised if they can rally the troops against seemingly insurmountable odds.</p>
+
+    <p>The symbol of Aries is the Ram, and that's both good and bad news. Impulsive Aries might be tempted to ram their ideas down everyone's throats without even bothering to ask if they want to know. It's these times when you may wish Aries' symbol were a more subdued creature, more lamb than ram perhaps. You're not likely to convince the Ram to soften up; these folks are blunt and to the point. Along with those qualities comes the sheer force of the Aries nature, a force that can actually accomplish a great deal. Much of Aries' drive to compete and to win comes from its Cardinal Quality. Cardinal Signs love to get things going, and Aries exemplifies this even better than Cancer, Libra or Capricorn.</p>
+
+    <p class="text-right">
+        <a href="singlepost.html" class="text-right">
+            continue reading...
+        </a>
+    </p>
+
+    <hr>
+</article>
+*/
